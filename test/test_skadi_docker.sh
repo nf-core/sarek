4d317954c6197b0cfa3895ad7a9d296652643f61
MaxUlysse <max@ithake.eu>
2017-03-16 16:58:16 +0100

    add which test is running

--- test/test_skadi_docker.sh
+++ test/test_skadi_docker.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-
-nextflow run . -profile docker_skadi --test --steps preprocessing,MultiQC
-nextflow run . -profile docker_skadi --test --steps realign,MultiQC
-nextflow run . -profile docker_skadi --test --steps recalibrate,MultiQC
-nextflow run . -profile docker_skadi --test --steps skipPreprocessing,HaplotypeCaller,MuTect1,MuTect2
+echo "Starting Nextflow... Command:"
+echo "nextflow run . -profile docker_skadi --test --steps preprocessing --tools MultiQC"
+echo "-----"
+nextflow run . -profile docker_skadi --test --steps preprocessing --tools MultiQC
+echo "Starting Nextflow... Command:"
+echo "nextflow run . -profile docker_skadi --test --steps realign --tools MultiQC"
+echo "-----"
+nextflow run . -profile docker_skadi --test --steps realign --tools MultiQC
+echo "Starting Nextflow... Command:"
+echo "nextflow run . -profile docker_skadi --test --steps recalibrate --tools MultiQC"
+echo "-----"
+nextflow run . -profile docker_skadi --test --steps recalibrate --tools MultiQC
+echo "Starting Nextflow... Command:"
+echo "nextflow run . -profile docker_skadi --test --steps skipPreprocessing --tools HaplotypeCaller,MuTect1,MuTect2"
+echo "-----"
+nextflow run . -profile docker_skadi --test --steps skipPreprocessing --tools HaplotypeCaller,MuTect1,MuTect2
