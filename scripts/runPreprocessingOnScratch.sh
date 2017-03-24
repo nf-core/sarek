c9d438d134cffdf2164f2621f883e6b4d99e0c53
Szilveszter Juhos <szilveszter.juhos@scilifelab.se>
2017-03-24 14:35:56 +0100

    fixed new syntax

--- scripts/runPreprocessingOnScratch.sh
+++ scripts/runPreprocessingOnScratch.sh
@@ -6,4 +6,4 @@ module load Nextflow
 export NXF_TEMP=/scratch
 export NXF_LAUNCHBASE=/scratch
 export NXF_WORK=/scratch
-nextflow run /home/szilva/CAW/main.nf -c /home/szilva/CAW/nextflow.config -profile localhost --project sens2016004 --verbose --steps preprocessing --sample $1
+nextflow run /home/szilva/CAW/main.nf -c /home/szilva/CAW/nextflow.config -profile localhost --project sens2016004 --verbose --step preprocessing --sample $1
