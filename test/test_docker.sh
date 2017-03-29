558911c3245b19484811e58632da7030c9afb503
MaxUlysse <max@ithake.eu>
2017-03-29 10:41:23 +0200

    add Strelka to tests

--- test/test_docker.sh
+++ test/test_docker.sh
@@ -8,14 +8,14 @@ echo "nextflow run . -profile docker --test --step preprocessing --tools MultiQC
 echo "-----"
 nextflow run . -profile docker --test --step preprocessing --tools MultiQC
 echo "Starting Nextflow... Command:"
-echo "nextflow run . -profile docker --test --step realign --tools MultiQC "
+echo "nextflow run . -profile docker --test --step realign --tools MultiQC"
 echo "-----"
 nextflow run . -profile docker --test --step realign --tools MultiQC
 echo "Starting Nextflow... Command:"
-echo "nextflow run . -profile docker --test --step recalibrate --tools MultiQC "
+echo "nextflow run . -profile docker --test --step recalibrate --tools MultiQC"
 echo "-----"
 nextflow run . -profile docker --test --step recalibrate --tools MultiQC
 echo "Starting Nextflow... Command:"
-echo "nextflow run . -profile docker --test --step skipPreprocessing --tools HaplotypeCaller,MuTect1,MuTect2 "
+echo "nextflow run . -profile docker --test --step skipPreprocessing --tools HaplotypeCaller,MuTect1,MuTect2,Strelka"
 echo "-----"
-nextflow run . -profile docker --test --step skipPreprocessing --tools HaplotypeCaller,MuTect1,MuTect2
+nextflow run . -profile docker --test --step skipPreprocessing --tools HaplotypeCaller,MuTect1,MuTect2,Strelka
