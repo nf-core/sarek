df8c16c1811eb06b0b97a697054e92092182d9c7
MaxUlysse <max@ithake.eu>
2017-03-29 15:25:33 +0200

    correct typo in test

--- test/test_docker.sh
+++ test/test_docker.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 echo "Starting Nextflow... Command:"
-echo "nextflow run buildReferences.nf -profile docker --storeDirectory smallGRCh37"
+echo "./nextflow run buildReferences.nf -profile docker --storeDirectory smallGRCh37"
 echo "-----"
-nextflow run buildReferences.nf -profile docker --storeDirectory smallGRCh37
+./nextflow run buildReferences.nf -profile docker --storeDirectory smallGRCh37
 echo "Starting Nextflow... Command:"
-echo "nextflow run . -profile docker --test --step preprocessing --tools MultiQC"
+echo "./nextflow run . -profile docker --test --step preprocessing --tools MultiQC"
 echo "-----"
-nextflow run . -profile docker --test --step preprocessing --tools MultiQC
+./nextflow run . -profile docker --test --step preprocessing --tools MultiQC
 echo "Starting Nextflow... Command:"
-echo "nextflow run . -profile docker --test --step realign"
+echo "./nextflow run . -profile docker --test --step realign"
 echo "-----"
-nextflow run . -profile docker --test --step realign
+./nextflow run . -profile docker --test --step realign
 echo "Starting Nextflow... Command:"
-echo "nextflow run . -profile docker --test --step recalibrate"
+echo "./nextflow run . -profile docker --test --step recalibrate"
 echo "-----"
-nextflow run . -profile docker --test --step recalibrate
+./nextflow run . -profile docker --test --step recalibrate
 echo "Starting Nextflow... Command:"
-echo "nextflow run . -profile docker --test --step skipPreprocessing --tools HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff"
+echo "./nextflow run . -profile docker --test --step skipPreprocessing --tools HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff"
 echo "-----"
-nextflow run . -profile docker --test --step skipPreprocessing --tools HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff
+./nextflow run . -profile docker --test --step skipPreprocessing --tools HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff
