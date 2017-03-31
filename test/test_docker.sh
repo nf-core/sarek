718df1f27f3dfe5c1830aef2f3c5f2fd64086b62
MaxUlysse <max@ithake.eu>
2017-03-31 16:00:26 +0200

    no need to erase everything

--- test/test_docker.sh
+++ test/test_docker.sh
@@ -4,33 +4,25 @@ echo "./nextflow run buildReferences.nf -profile testing --storeDirectory smallG
 echo "-----"
 ./nextflow run buildReferences.nf -profile testing --storeDirectory smallGRCh37
 echo "Cleaning up docker images:"
-echo "docker rmi -f $(docker images -q)"
+echo "docker rmi -f maxulysse/igvtools:1.1"
 echo "-----"
-docker rmi -f $(docker images -q)
+docker rmi -f maxulysse/igvtools:1.1
 echo "Starting Nextflow... Command:"
 echo "./nextflow run . -profile testing --test --step preprocessing --tools MultiQC"
 echo "-----"
 ./nextflow run . -profile testing --test --step preprocessing --tools MultiQC
 echo "Cleaning up docker images:"
-echo "docker rmi -f $(docker images -q)"
+echo "docker rmi -f maxulysse/mapreads:1.1 maxulysse/samtools:1.1 maxulysse/picard:1.1 maxulysse/fastqc:1.1"
 echo "-----"
-docker rmi -f $(docker images -q)
+docker rmi -f maxulysse/mapreads:1.1 maxulysse/samtools:1.1 maxulysse/picard:1.1 maxulysse/fastqc:1.1
 echo "Starting Nextflow... Command:"
 echo "./nextflow run . -profile testing --test --step realign"
 echo "-----"
 ./nextflow run . -profile testing --test --step realign
-echo "Cleaning up docker images:"
-echo "docker rmi -f $(docker images -q)"
-echo "-----"
-docker rmi -f $(docker images -q)
 echo "Starting Nextflow... Command:"
 echo "./nextflow run . -profile testing --test --step recalibrate"
 echo "-----"
 ./nextflow run . -profile testing --test --step recalibrate
-echo "Cleaning up docker images:"
-echo "docker rmi -f $(docker images -q)"
-echo "-----"
-docker rmi -f $(docker images -q)
 echo "Starting Nextflow... Command:"
 echo "./nextflow run . -profile testing --test --step skipPreprocessing --tools HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff,VEP"
 echo "-----"
