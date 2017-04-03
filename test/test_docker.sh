2ad322525b0ec0b002d4a8631aa8ce17e95a89a4
MaxUlysse <max@ithake.eu>
2017-04-03 11:08:54 +0200

    simplifying test

--- test/test_docker.sh
+++ test/test_docker.sh
@@ -24,14 +24,6 @@ echo "./nextflow run . -profile testing --test --step recalibrate"
 echo "-----"
 ./nextflow run . -profile testing --test --step recalibrate
 echo "Starting Nextflow... Command:"
-echo "./nextflow run . -profile testing --test --step skipPreprocessing --tools HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff"
+echo "./nextflow run . -profile testing --test --step skipPreprocessing --tools HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff,VEP"
 echo "-----"
-./nextflow run . -profile testing --test --step skipPreprocessing --tools HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff
-echo "Cleaning up docker images:"
-echo "docker rmi -f maxulysse/igvtools:1.1"
-echo "-----"
-docker rmi -f maxulysse/igvtools:1.1 maxulysse/multiqc:1.1 maxulysse/gatk:1.1 maxulysse/strelka:1.1 maxulysse/snpeff:1.1
-echo "Starting Nextflow... Command:"
-echo "./nextflow run . -profile testing --test --step skipPreprocessing --tools MuTect1,VEP"
-./nextflow run . -profile testing --test --step skipPreprocessing --tools MuTect1,VEP
-echo "-----"
+./nextflow run . -profile testing --test --step skipPreprocessing --tools HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff,VEP
