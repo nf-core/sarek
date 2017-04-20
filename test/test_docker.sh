9bc10b6f041aa56d5a504d6cdbf5fdd96a4fd63a
MaxUlysse <max@ithake.eu>
2017-04-20 13:20:21 +0200

    update test for bamQC

--- test/test_docker.sh
+++ test/test_docker.sh
@@ -8,9 +8,9 @@ echo "docker rmi -f maxulysse/igvtools:1.1"
 echo "-----"
 docker rmi -f maxulysse/igvtools:1.1
 echo "Starting Nextflow... Command:"
-echo "./nextflow run . -profile testing --test --step preprocessing --tools MultiQC"
+echo "./nextflow run . -profile testing --test --step preprocessing --tools bamQC,MultiQC"
 echo "-----"
-./nextflow run . -profile testing --test --step preprocessing --tools MultiQC
+./nextflow run . -profile testing --test --step preprocessing --tools bamQC,MultiQC
 echo "Cleaning up docker images:"
 echo "docker rmi -f maxulysse/mapreads:1.1 maxulysse/samtools:1.1 maxulysse/picard:1.1 maxulysse/fastqc:1.1"
 echo "-----"
