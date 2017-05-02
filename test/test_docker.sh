670a42e9776a2d7777988f33e1e883f4e485ec0c
MaxUlysse <max@ithake.eu>
2017-05-02 14:59:59 +0200

    move nextflow to bin

--- test/test_docker.sh
+++ test/test_docker.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 echo "Starting Nextflow... Command:"
-echo "./nextflow run buildReferences.nf -profile testing --download"
+echo "nextflow run buildReferences.nf -profile testing --download"
 echo "-----"
-./nextflow run buildReferences.nf -profile testing --download
+nextflow run buildReferences.nf -profile testing --download
 echo "Cleaning up docker images:"
 echo "docker rmi -f maxulysse/igvtools:1.1"
 echo "-----"
 docker rmi -f maxulysse/igvtools:1.1
 echo "Starting Nextflow... Command:"
-echo "./nextflow run . -profile testing --test --step preprocessing --tools MultiQC"
+echo "nextflow run . -profile testing --test --step preprocessing --tools MultiQC"
 echo "-----"
-./nextflow run . -profile testing --test --step preprocessing --tools MultiQC
+nextflow run . -profile testing --test --step preprocessing --tools MultiQC
 echo "Cleaning up docker images:"
 echo "docker rmi -f maxulysse/mapreads:1.1 maxulysse/samtools:1.1 maxulysse/picard:1.1 maxulysse/fastqc:1.1"
 echo "-----"
 docker rmi -f maxulysse/mapreads:1.1 maxulysse/samtools:1.1 maxulysse/picard:1.1 maxulysse/fastqc:1.1
 echo "Starting Nextflow... Command:"
-echo "./nextflow run . -profile testing --step realign"
+echo "nextflow run . -profile testing --step realign"
 echo "-----"
-./nextflow run . -profile testing --step realign
+nextflow run . -profile testing --step realign
 echo "Starting Nextflow... Command:"
-echo "./nextflow run . -profile testing --step recalibrate"
+echo "nextflow run . -profile testing --step recalibrate"
 echo "-----"
-./nextflow run . -profile testing --step recalibrate
+nextflow run . -profile testing --step recalibrate
 echo "Starting Nextflow... Command:"
-echo "./nextflow run . -profile testing --step skipPreprocessing --tools HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff,VEP,VarDict"
+echo "nextflow run . -profile testing --step skipPreprocessing --tools HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff,VEP,VarDict"
 echo "-----"
-./nextflow run . -profile testing --step skipPreprocessing --tools FreeBayes,HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff,VarDict
+nextflow run . -profile testing --step skipPreprocessing --tools FreeBayes,HaplotypeCaller,MultiQC,MuTect1,MuTect2,Strelka,snpEff,VarDict
