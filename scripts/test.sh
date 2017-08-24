e982e3f22f7d68384ebd03d6daf502262e86129c
Maxime Garcia <max.u.garcia@gmail.com>
2017-08-24 17:08:43 +0200

    more parralelization

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,20 +13,29 @@ nf_test buildReferences.nf --download
 if [ $TEST = MAPPING ]
 then
   nf_test . --test --step preprocessing
+fi
+
+if [ $TEST = REALIGN ]
+then
+  nf_test . --test --step preprocessing
   nf_test . --step realign --noReports
   nf_test . --step realign --tools HaplotypeCaller
   nf_test . --step realign --tools HaplotypeCaller --noReports --noGVCF
+fi
+
+if [ $TEST = RECALIBRATE ]
+then
+  nf_test . --test --step preprocessing
   nf_test . --step recalibrate --noReports
   nf_test . --step recalibrate --tools FreeBayes,HaplotypeCaller,MuTect1,MuTect2,Strelka
   # Test whether restarting from an already recalibrated BAM works
   nf_test . --step skipPreprocessing --tools Strelka --noReports
 fi
 
-if [ $TEST = VARIANTCALLING ]
+if [ $TEST = ANNOTATE ]
 then
   nf_test . --step preprocessing --sample data/tsv/tiny-manta.tsv --tools Manta
-  nf_test . --test --step preprocessing
-  nf_test . --step skipPreprocessing --tools MuTect2,snpEff,VEP --noReports
+  nf_test . --test --step preprocessing --tools MuTect2,Strelka,snpEff,VEP --noReports
   nf_test . --step annotate --tools snpEff,VEP --annotateTools Strelka
   nf_test . --step annotate --tools snpEff --annotateVCF VariantCalling/Manta/Manta_9876T_vs_1234N.diploidSV.vcf,VariantCalling/Manta/Manta_9876T_vs_1234N.somaticSV.vcf --noReports
 fi
