cc8dbda56962cac9db5c2335b15f3261128c6413
Maxime Garcia <max.u.garcia@gmail.com>
2017-08-24 15:43:28 +0200

    only one script

---
+++ scripts/test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -xeuo pipefail
+PROFILE=$1
+
+function nf_test() {
+  echo "$(tput setaf 1)nextflow run $@ -profile $PROFILE -resume --verbose$(tput sgr0)"
+  nextflow run "$@" -profile $PROFILE -resume --verbose
+}
+
+nf_test buildReferences.nf --download
+
+# Clean up images
+if [ $PROFILE = docker ]
+then
+  docker rmi -f maxulysse/igvtools:1.1
+else
+  rm -rf work/singularity/igvtools-1.1.img
+fi
+
+nf_test . --step preprocessing --sample data/tsv/tiny-manta.tsv --tools Manta
+nf_test . --test --step preprocessing
+
+# Clean up images
+if [ $PROFILE = docker ]
+then
+  docker rmi -f maxulysse/fastqc:1.1 maxulysse/mapreads:1.1 maxulysse/picard:1.1 maxulysse/runmanta:1.1
+else
+  rm -rf work/singularity/fastqc-1.1.img work/singularity/mapreads-1.1.img work/singularity/picard-1.1.img work/singularity/runmanta-1.1.img
+fi
+
+nf_test . --step realign --noReports
+nf_test . --step realign --tools HaplotypeCaller
+nf_test . --step realign --tools HaplotypeCaller --noReports --noGVCF
+nf_test . --step recalibrate --noReports
+nf_test . --step recalibrate --tools FreeBayes,HaplotypeCaller,MuTect1,MuTect2,Strelka
+# Test whether restarting from an already recalibrated BAM works
+nf_test . --step skipPreprocessing --tools Strelka --noReports
+
+# Clean up images
+if [ $PROFILE = docker ]
+then
+  docker rmi -f maxulysse/concatvcf:1.1 maxulysse/freebayes:1.1 maxulysse/gatk:1.0 maxulysse/gatk:1.1 maxulysse/mutect1:1.1 maxulysse/samtools:1.1 maxulysse/strelka:1.1
+else
+  rm -rf work/singularity/concatvcf-1.1.img work/singularity/freebayes-1.1.img work/singularity/gatk-1.0.img work/singularity/gatk-1.1.img work/singularity/mutect1-1.1.img work/singularity/samtools-1.1.img work/singularity/strelka-1.1.img
+fi
+
+nf_test . --step skipPreprocessing --tools MuTect2,snpEff,VEP --noReports
+nf_test . --step annotate --tools snpEff,VEP --annotateTools Strelka
+nf_test . --step annotate --tools snpEff --annotateVCF VariantCalling/Manta/Manta_9876T_vs_1234N.diploidSV.vcf,VariantCalling/Manta/Manta_9876T_vs_1234N.somaticSV.vcf --noReports
