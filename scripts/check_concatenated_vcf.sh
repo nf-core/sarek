9ec1022100d04a43fb9e58e0c0689ac60b5d1c8a
malinlarsson <malin.larsson@liu.se>
2017-08-30 15:16:26 +0200

    removing unused stuff

--- scripts/check_concatenated_vcf.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-#Input data
-vcf=$1
-for chr in `seq 1 22` X Y M
-do
-echo "chr "$chr
-grep "^$chr" $vcf | wc -l
-done
--- scripts/runHapltypeCallerOnScratch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xl
-#SBATCH -A b2011185
-#SBATCH -p node
-#SBATCH -t 168:00:00
-module load Nextflow
-export NXF_TEMP=/scratch
-export NXF_LAUNCHBASE=/scratch
-export NXF_WORK=/scratch
-nextflow run /proj/b2011185/nobackup/private/analysis/CAW/CAW/main.nf --project b2011185 --step skippreprocessing --tools haplotypecaller --genome GRCh37 --sample $1
