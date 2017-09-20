b63ed28d5f8ea3ad90734c6afa4f5a038682afea
Maxime Garcia <max.u.garcia@gmail.com>
2017-09-20 16:55:25 +0200

    update tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -77,6 +77,7 @@ fi
 
 if [[ "$TEST" = "ANNOTATEVEP" ]] || [[ "$TEST" = "ALL" ]]
 then
+  nf_test . --step preprocessing --sample data/tsv/tiny-single-manta.tsv --tools Manta
   nf_test . --step preprocessing --sample data/tsv/tiny-manta.tsv --tools Manta
   nf_test . --step preprocessing --sample $SAMPLE --tools MuTect2,Strelka
 
@@ -88,13 +89,14 @@ then
   then
     rm -rf work/singularity/fastqc-1.1.img work/singularity/gatk-1.0.img work/singularity/gatk-1.1.img work/singularity/mapreads-1.1.img work/singularity/picard-1.1.img work/singularity/runmanta-1.1.img work/singularity/samtools-1.1.img work/singularity/strelka-1.1.img
   fi
-  nf_test . --step annotate --tools VEP --annotateTools Strelka
+  nf_test . --step annotate --tools VEP --annotateTools Manta,Strelka
   nf_test . --step annotate --tools VEP --annotateVCF VariantCalling/Manta/Manta_9876T_vs_1234N.diploidSV.vcf,VariantCalling/Manta/Manta_9876T_vs_1234N.somaticSV.vcf --noReports
   nf_test . --step annotate --tools VEP --annotateVCF VariantCalling/Manta/Manta_9876T_vs_1234N.diploidSV.vcf --noReports
 fi
 
 if [[ "$TEST" = "ANNOTATESNPEFF" ]] || [[ "$TEST" = "ALL" ]]
 then
+  nf_test . --step preprocessing --sample data/tsv/tiny-single-manta.tsv --tools Manta
   nf_test . --step preprocessing --sample data/tsv/tiny-manta.tsv --tools Manta
   nf_test . --step preprocessing --sample $SAMPLE --tools MuTect2,Strelka
 
@@ -106,7 +108,7 @@ then
   then
     rm -rf work/singularity/fastqc-1.1.img work/singularity/gatk-1.0.img work/singularity/gatk-1.1.img work/singularity/mapreads-1.1.img work/singularity/picard-1.1.img work/singularity/runmanta-1.1.img work/singularity/samtools-1.1.img work/singularity/strelka-1.1.img
   fi
-  nf_test . --step annotate --tools snpEff --annotateTools Strelka
+  nf_test . --step annotate --tools snpEff --annotateTools Manta,Strelka
   nf_test . --step annotate --tools snpEff --annotateVCF VariantCalling/Manta/Manta_9876T_vs_1234N.diploidSV.vcf,VariantCalling/Manta/Manta_9876T_vs_1234N.somaticSV.vcf --noReports
   nf_test . --step annotate --tools snpEff --annotateVCF VariantCalling/Manta/Manta_9876T_vs_1234N.diploidSV.vcf --noReports
 fi
