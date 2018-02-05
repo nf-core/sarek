62db197e7ce7e5ad86dbc706f88038980ca06489
Maxime Garcia <max.u.garcia@gmail.com>
2018-02-05 14:44:54 +0100

    Get Somatic part out

--- scripts/test.sh
+++ scripts/test.sh
@@ -63,57 +63,26 @@ fi
 
 if [[ ALL,MAPPING,ONLYQC,REALIGN,RECALIBRATE =~ $TEST ]]
 then
-  nf_test . --step mapping --sampleDir data/tiny/tiny/normal
-  nf_test . --step mapping --sample $SAMPLE
+  nf_test main.nf --step mapping --sampleDir data/tiny/tiny/normal
+  nf_test main.nf --step mapping --sample $SAMPLE
 fi
 
 if [[ ALL,ONLYQC =~ $TEST ]]
 then
-  nf_test . --step mapping --sample $SAMPLE --tools Strelka --noReports
-  nf_test . --step variantCalling --tools Strelka --onlyQC
+  nf_test main.nf --step mapping -- --sample $SAMPLE --noReports
 fi
 
 if [[ ALL,REALIGN =~ $TEST ]]
 then
-  nf_test . --step realign --noReports
-  nf_test . --step realign --tools HaplotypeCaller
-  nf_test . --step realign --tools HaplotypeCaller --noReports --noGVCF
+  nf_test main.nf --step realign --noReports
 fi
 
 if [[ ALL,RECALIBRATE =~ $TEST ]]
 then
-  nf_test . --step recalibrate --noReports
-  nf_test . --step recalibrate --tools FreeBayes,HaplotypeCaller,MuTect1,MuTect2,Strelka
-  # Test whether restarting from an already recalibrated BAM works
-  nf_test . --step variantCalling --tools Strelka --noReports
-fi
-
-if [[ ALL,ANNOTATESNPEFF,ANNOTATEVEP =~ $TEST ]]
-then
-  if [[ $TEST = ANNOTATESNPEFF ]]
-  then
-    ANNOTATOR=snpEFF
-  elif [[ $TEST = ANNOTATEVEP ]]
-  then
-    ANNOTATOR=VEP
-  elif  [[ $TEST = ALL ]]
-  then
-    ANNOTATOR=snpEFF,VEP
-  fi
-  if [[ $PROFILE == docker ]] && [[ $TRAVIS == true ]]
-  then
-    docker rmi -f maxulysse/caw:latest
-    docker rmi -f maxulysse/picard:latest
-  elif [[ $PROFILE == singularity ]] && [[ $TRAVIS == true ]]
-  then
-    rm -rf work/singularity/caw-latest.img
-    rm -rf work/singularity/picard-latest.img
-  fi
-  nf_test . --step annotate --tools ${ANNOTATOR} --annotateVCF data/tiny/vcf/Strelka_1234N_variants.vcf.gz --noReports
-  nf_test . --step annotate --tools ${ANNOTATOR} --annotateVCF data/tiny/vcf/Strelka_1234N_variants.vcf.gz,data/tiny/vcf/Strelka_9876T_variants.vcf.gz --noReports
+  nf_test main.nf --step recalibrate --noReports
 fi
 
 if [[ ALL,BUILDCONTAINERS =~ $TEST ]] && [[ $PROFILE == docker ]]
 then
-  nf_test buildContainers.nf --docker --containers caw,fastqc,gatk,igvtools,multiqc,mutect1,picard,qualimap,runallelecount,r-base,snpeff
+  nf_test buildContainers.nf --docker --containers caw,fastqc,gatk,igvtools,picard,qualimap
 fi
