2f70b33d99c91ec3a6ccedfde6b10c048a67a2f0
Maxime Garcia <max.u.garcia@gmail.com>
2018-03-06 16:09:06 +0100

    remove old files

--- data/tiny/getfastq.py
+++
@@ -1,5 +0,0 @@
-from Bio.SeqIO.QualityIO import FastqGeneralIterator
-import sys 
-ids = set(x[:-1] for x in open(sys.argv[1]))
-for name, seq, qual in FastqGeneralIterator(sys.stdin):
-    if (name in ids): print("@%s\n%s\n+\n%s" % (name, seq, qual))
