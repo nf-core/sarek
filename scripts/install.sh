64647be6a768a268df8fb693ea26d36d094bec0b
Maxime Garcia <max.u.garcia@gmail.com>
2018-05-28 16:32:52 +0200

    remove sudo apt-get update

--- scripts/install.sh
+++ scripts/install.sh
@@ -30,7 +30,6 @@ fi
 # Install Singularity
 if [[ "$TOOL" = singularity ]] || [[ "$TOOL" = all ]]
 then
-  sudo apt-get update
   sudo apt-get install squashfs-tools
   cd $HOME
   wget https://github.com/singularityware/singularity/releases/download/$SGT_VER/singularity-$SGT_VER.tar.gz
