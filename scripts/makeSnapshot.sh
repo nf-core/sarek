3a6fe65c635de00ae258da566d7c72ef2a91db9b
Maxime Garcia <max.u.garcia@gmail.com>
2018-07-17 15:18:56 +0200

    update maskSnapshot script to optionnaly archive with submodule

--- scripts/makeSnapshot.sh
+++ scripts/makeSnapshot.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 set -euo pipefail
+
+TEST=false
 NAME=Sarek-$(git describe --tags)
-git archive HEAD --prefix=$NAME/ | gzip > $NAME.tar.gz
+
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+    -i|-t|--include-test-data)
+    TEST=true
+    shift # past argument
+    ;;
+    *) # unknown option
+    shift # past argument
+    ;;
+  esac
+done
+
+if [[ $TEST == false ]]
+then
+  echo "Archiving Sarek without test data"
+  git archive HEAD --prefix=$NAME/ | gzip > $NAME.tar.gz
+else
+  echo "Archiving Sarek with test data"
+  git-archive-all --prefix=$NAME/ --force-submodules $NAME.tar.gz
+fi
+
 echo "Wrote $NAME.tar.gz"
