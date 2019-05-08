c117515dca05d108294c025b03e501b3ebb89c34
MaxUlysse <max.u.garcia@gmail.com>
2019-05-08 15:20:28 +0200

    improve script

--- bin/build_reference.sh
+++ bin/build_reference.sh
@@ -26,10 +26,13 @@ do
 done
 
 # Always download test data
+rm -rf data
 git clone --single-branch --branch sarek https://github.com/nf-core/test-datasets.git data
 
 # Build references for smallGRCh37
-if [[ BUILD == true ]] && [[ $TEST != ANNOTATESNPEFF ]] && [[ $TEST != ANNOTATEVEP ]]
+if [[ BUILD ]] && [[ $TEST != ANNOTATESNPEFF ]] && [[ $TEST != ANNOTATEVEP ]]
 then
+  rm -rf references
   nextflow run build.nf -profile docker --genome smallGRCh37 --refdir data/reference --outdir references --publishDirMode link --max_memory 7.GB --max_cpus 2 -ansi-log false
+  rm -rf .nextflow* references/pipeline_info work
 fi
