f7231af2e013594a1fbab31e86a5bb5a496de670
Malin Larsson <malin.larsson@liu.se>
2020-01-10 17:23:22 +0100

    passing arguments to run_ascat.R in python style so that it is possible to set either purity or ploidy and use the default of the other. Note - this version of run_ascat.R is not compatible with old main.nf!

--- bin/run_ascat.r
+++ bin/run_ascat.r
@@ -1,73 +1,96 @@
 q#!/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
-if(length(args)<8) {
-    stop("No input files supplied\n\nUsage:\nRscript run_ascat.r tumor_baf tumor_logr normal_baf normal_logr tumor_sample_name baseDir gcfile gender purity ploidy\n\n")
-} else {
-    tumorbaf = args[1]
-    tumorlogr = args[2]
-    normalbaf = args[3]
-    normallogr = args[4]
-    tumorname = args[5]
-    baseDir = args[6]
-    gcfile = args[7]
-    gender = args[8]
-    purity = args[9]
-    ploidy = args[10]
-}
+library("optparse")
+option_list = list(
+    make_option("--tumorbaf", type="character", default=NULL,
+              help="tumor BAF file", metavar="character"),
+              make_option("--tumorlogr", type="character", default=NULL,
+              help="tumor LogR file", metavar="character"),
+              make_option("--normalbaf", type="character", default=NULL,
+              help="normal BAF file", metavar="character"),
+              make_option("--normallogr", type="character", default=NULL,
+              help="normal LogR file", metavar="character"),
+              make_option("--tumorname", type="character", default=NULL,
+              help="name of tumor sample file", metavar="character"),
+              make_option("--basedir", type="character", default=NULL,
+              help="main Sarek directory for sample", metavar="character"),
+              make_option("--gcfile", type="character", default=NULL,
+              help="GC correction file", metavar="character"),
+              make_option("--gender", type="character", default=NULL,
+              help="gender on format XX or XY", metavar="character"),
+              make_option("--purity", type="character", default=NULL,
+              help="override Ascat purity parameter (rho_manual) ", metavar="character"),
+              make_option("--ploidy", type="character", default=NULL,
+              help="override Ascat ploidy parameter (psi_manual)", metavar="character"))
 
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+if(is.null(opt$tumorbaf) || is.null(opt$tumorlogr) || is.null(opt$normalbaf) || is.null(opt$normallogr) || is.null(opt$tumorname) || is.null(opt$basedir) || is.null(opt$gcfile) || is.null(opt$gender))  {
+    print_help(opt_parser)
+    stop("At least one of the required arguments missing.", call.=FALSE)
+    } 
+    
 library(ASCAT)
 
 if(!require(RColorBrewer)){
     source("http://bioconductor.org/biocLite.R")
-    biocLite("RColorBrewer", suppressUpdates=TRUE, lib="$baseDir/scripts")
+    biocLite("RColorBrewer", suppressUpdates=TRUE, lib="$opt$basedir/scripts")
     library(RColorBrewer)
 }
 options(bitmapType='cairo')
 
-if (exists(purity) and exists(ploidy)){
+if (!is.null(opt$ploidy) && !is.null(opt$purity)){
     fileConn<-file("test.txt")
-    writeLines(c("purity",,"  ", ploidy"), fileConn)
+    writeLines(c(opt$ploidy,"  ", opt$purity), fileConn)
     close(fileConn)
 }
 
 #Load the  data
-#ascat.bc <- ascat.loadData(Tumor_LogR_file=tumorlogr, Tumor_BAF_file=tumorbaf, Germline_LogR_file=normallogr, Germline_BAF_file=normalbaf, chrs = c(1:22,"X","Y"), gender = gender, sexchromosomes = c("X", "Y"))
+#ascat.bc <- ascat.loadData(Tumor_LogR_file=opt$tumorlogr, Tumor_BAF_file=opt$tumorbaf, Germline_LogR_file=opt$normallogr, Germline_BAF_file=opt$normalbaf, chrs = c(1:22,"X","Y"), opt$gender = opt$gender, sexchromosomes = c("X", "Y"))
 
-if(gender=="XY"){
-    ascat.bc = ascat.loadData(Tumor_LogR_file = tumorlogr, Tumor_BAF_file = tumorbaf, Germline_LogR_file = normallogr, Germline_BAF_file = normalbaf, gender = gender, chrs = c(1:22,"X","Y"), sexchromosomes = c("X","Y"))
+if(opt$gender=="XY"){
+    ascat.bc = ascat.loadData(Tumor_LogR_file = opt$tumorlogr, Tumor_BAF_file = opt$tumorbaf, Germline_LogR_file = opt$normallogr, Germline_BAF_file = opt$normalbaf, gender = opt$gender, chrs = c(1:22,"X","Y"), sexchromosomes = c("X","Y"))
 } else {
-    ascat.bc = ascat.loadData(Tumor_LogR_file = tumorlogr, Tumor_BAF_file = tumorbaf, Germline_LogR_file = normallogr, Germline_BAF_file = normalbaf, gender = gender, chrs = c(1:22,"X"), sexchromosomes = c("X"))
+    ascat.bc = ascat.loadData(Tumor_LogR_file = opt$tumorlogr, Tumor_BAF_file = opt$tumorbaf, Germline_LogR_file = opt$normallogr, Germline_BAF_file = opt$normalbaf, gender = opt$gender, chrs = c(1:22,"X"), sexchromosomes = c("X"))
 
 }
 
 
 #GC wave correction
-ascat.bc = ascat.GCcorrect(ascat.bc, gcfile)
+#ascat.bc = ascat.GCcorrect(ascat.bc, opt$gcfile)
 
 #Plot the raw data
-ascat.plotRawData(ascat.bc)
+#ascat.plotRawData(ascat.bc)
 
 #Segment the data
-ascat.bc <- ascat.aspcf(ascat.bc)
+#ascat.bc <- ascat.aspcf(ascat.bc)
 
 #Plot the segmented data
-ascat.plotSegmentedData(ascat.bc)
+#ascat.plotSegmentedData(ascat.bc)
 
 #Run ASCAT to fit every tumor to a model, inferring ploidy, normal cell contamination, and discrete copy numbers
 #If psi and rho are manually set:
-if (exists(purity) and exists(ploidy)){
-    ascat.output <- ascat.runAscat(ascat.bc, gamma=1, rho_manual=purity, psi_manual=ploidy)
+if (!is.null(opt$purity) && !is.null(opt$ploidy)){
+  ascat.plotRawData(ascat.bc)
+    #ascat.output <- ascat.runAscat(ascat.bc, gamma=1, rho_manual=opt$purity, psi_manual=opt$ploidy)
+} else if(!is.null(opt$purity) && is.null(opt$ploidy)){
+  ascat.plotRawData(ascat.bc)
+    #ascat.output <- ascat.runAscat(ascat.bc, gamma=1, rho_manual=opt$purity)
+} else if(!is.null(opt$ploidy) && is.null(opt$purity)){
+  ascat.plotRawData(ascat.bc)
+    #ascat.output <- ascat.runAscat(ascat.bc, gamma=1, psi_manual=opt$ploidy)
 } else {
-    ascat.output <- ascat.runAscat(ascat.bc, gamma=1)
+  ascat.plotRawData(ascat.bc)
+    #ascat.output <- ascat.runAscat(ascat.bc, gamma=1)
 }
 
 
 #Write out segmented regions (including regions with one copy of each allele)
-#write.table(ascat.output$segments, file=paste(tumorname, ".segments.txt", sep=""), sep="\t", quote=F, row.names=F)
+#write.table(ascat.output$segments, file=paste(opt$tumorname, ".segments.txt", sep=""), sep="\t", quote=F, row.names=F)
 
 #Write out CNVs in bed format
 cnvs=ascat.output$segments[2:6]
-write.table(cnvs, file=paste(tumorname,".cnvs.txt",sep=""), sep="\t", quote=F, row.names=F, col.names=T)
+write.table(cnvs, file=paste(opt$tumorname,".cnvs.txt",sep=""), sep="\t", quote=F, row.names=F, col.names=T)
 
 #Write out purity and ploidy info
 summary <- tryCatch({
@@ -78,4 +101,4 @@ summary <- tryCatch({
 		}
 )
 colnames(summary) <- c("AberrantCellFraction","Ploidy")
-write.table(summary, file=paste(tumorname,".purityploidy.txt",sep=""), sep="\t", quote=F, row.names=F, col.names=T)
+write.table(summary, file=paste(opt$tumorname,".purityploidy.txt",sep=""), sep="\t", quote=F, row.names=F, col.names=T)
