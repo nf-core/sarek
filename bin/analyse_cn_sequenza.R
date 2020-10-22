bab5ac4ed8c379524cfc9854e6d981bec17a0e94
Chela James <chela.james@icr.ac.uk>
2020-10-22 18:15:47 +0100

    adding sequenza

---
+++ bin/analyse_cn_sequenza.R
@@ -0,0 +1,99 @@
+#!Rscript
+# Load libs:
+if (!require(sequenza)) stop("Package 'sequenza' missing\n.")
+
+args <- commandArgs(TRUE)
+
+input <- args[1]
+output_prefix <- args[2]
+gender <- args[3]
+
+
+# Function:
+sequenzaAnalysis <- function(input,
+                             output_prefix,
+                             window=1e6,
+                             overlap=1,
+                             gamma=80,
+                             kmin=10,
+                             min_reads=40,
+                             min_reads_normal=10,
+                             min_reads_baf=1,
+                             max_mut_types=1,
+                             breaks=NULL,
+                             assembly="hg19",
+                             weighted_mean=TRUE,
+                             normalization_method="mean",
+                             is_female=TRUE,
+                             segment_filter=3e6,
+                             ratio_priority=FALSE,
+                             method="baf",
+                             low_cell=0.1,
+                             up_cell=1,
+                             low_ploidy=1,
+                             up_ploidy=7,
+                             CNt_max=20) {
+  
+  # Define chromosomes to analyse (note these will subset to those that
+  # are available for sequenza:
+  chr_list <- c(1:22, "X")
+  if (gender != "XX")
+    chr_list <- c(chr_list, "Y")
+  chr_list <- c(chr_list, paste0("chr", chr_list))
+  
+  
+  # Extract sequenza data for model:
+  cat(sprintf("- Starting analysis for %s\n", input))
+  cat("- Calculating gc-stats\n")
+  is_gz_File <- grepl("[.]gz$", input)
+  gc_stats   <- gc.sample.stats(input, gz=is_gz_File)
+  
+  
+  cat("- Loading data\n")
+  modDat <- sequenza.extract(input,
+                             gz=is_gz_File,
+                             window=window,
+                             overlap=overlap,
+                             gamma=gamma,
+                             kmin=kmin,
+                             min.reads=min_reads,
+                             min.reads.normal=min_reads_normal,
+                             min.reads.baf=min_reads_baf,
+                             max.mut.types=max_mut_types,
+                             chromosome.list=chr_list,
+                             breaks=breaks,
+                             assembly=assembly,
+                             weighted.mean=weighted_mean,
+                             normalization.method=normalization_method,
+                             gc.stats=gc_stats)
+  
+  
+  # Fit the model:
+  cat("- Fitting the model\n")
+  cells  <- seq(low_cell, up_cell, 0.01)
+  plo    <- seq(low_ploidy, up_ploidy, 0.05)
+  fit    <- sequenza.fit(modDat,
+                         female=is_female,
+                         segment.filter=segment_filter,
+                         cellularity=cells,
+                         ploidy=plo,
+                         ratio.priority=ratio_priority,
+                         method=method)
+  
+  # Export the data:
+  cat("- Exporting results\n")
+  outName <- basename(output_prefix)
+  outDir  <- dirname(output_prefix)
+  sequenza.results(modDat,
+                   fit,
+                   outName,
+                   outDir,
+                   female=is_female,
+                   CNt.max=CNt_max,
+                   ratio.priority=ratio_priority)
+}
+
+is_named_param <- names(snakemake@params) != ""
+params_list <- c(input=snakemake@input[[1]], snakemake@params[is_named_param])
+do.call(sequenzaAnalysis, params_list)
+
