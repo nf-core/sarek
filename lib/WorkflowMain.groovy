a9781f75e54b4a393f1269057e0b6f5c58dcb969
Maxime U. Garcia <maxime.garcia@scilifelab.se>
2022-02-24 10:59:55 +0100

    Apply suggestions from code review
    Co-authored-by: FriederikeHanssen <Friederike.hanssen@qbic.uni-tuebingen.de>

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -71,6 +71,12 @@ class WorkflowMain {
 
         // Check AWS batch settings
         NfcoreTemplate.awsBatch(workflow, params)
+
+        // Check input has been provided
+        if (!params.input) {
+            log.error "Please provide an input samplesheet to the pipeline e.g. '--input samplesheet.csv'"
+            System.exit(1)
+        }
     }
 
     //
