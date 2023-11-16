b5fbd43030ae4de61900e3c13fd6a1175833f518
FriederikeHanssen <Friederike.hanssen@qbic.uni-tuebingen.de>
2023-11-16 09:16:38 +0000

    retrieve deposition ID

--- .github/workflows/upload.py
+++ .github/workflows/upload.py
@@ -4,7 +4,8 @@ import os
 headers = {"Content-Type": "application/json"}
 access_token = os.environ["ACCESS_TOKEN"]
 
-url = f"https://zenodo.org/api/deposit/depositions?access_token={access_token}"
+# TODO: replace sandbox link https://zenodo.org/api/deposit/depositions
+url = f"https://sandbox.zenodo.org/api/deposit/depositions?access_token={access_token}"
 
 filename = "*.vcf.gz"
 path = "../../variant_calling/%s" % filename
@@ -31,6 +32,9 @@ r = requests.post(url, data=json.dumps(data), headers=headers)
 r.status_code
 # 200
 
+r.json()["metadata"]["recid"]
+
+os.environ['DEPOSITION_ID'] = r.json()["metadata"]["recid"]
 # TODO add publication step
 #r = requests.post('https://zenodo.org/api/deposit/depositions/%s/actions/publish' % deposition_id,
 #                      params={'access_token': ACCESS_TOKEN} )
