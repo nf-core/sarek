fabed93335a4575bdbc8f0981e033e8378fb969b
FriederikeHanssen <Friederike.hanssen@qbic.uni-tuebingen.de>
2023-12-04 08:46:49 +0000

    try again to trigger upload

--- .github/workflows/upload.py
+++ .github/workflows/upload.py
@@ -6,27 +6,31 @@ access_token = os.environ["ACCESS_TOKEN"]
 workspace_directory = os.environ["GITHUB_WORKSPACE"]
 
 # TODO: replace sandbox link https://zenodo.org/api/deposit/depositions
-url = f"https://sandbox.zenodo.org/api/deposit/depositions?access_token={access_token}"
+# https://sandbox.zenodo.org/api/deposit/depositions?access_token={access_token}
+url = f"https://sandbox.zenodo.org/api/deposit/depositions/"
 
 # Create empty upload
+params = {'access_token': access_token}
 
+r = requests.post(url,
+                    params=params,
+                    json={},
+                    headers=headers)
 
+# Upload a new file
+bucket_url = r.json()["links"]["bucket"]
 
-# filename = "HCC1395N.strelka.genome.vcf.gz"
-# path = "./variant_calling/strelka/HCC1395N/%s" % filename
-# print(os.listdir('../../'))
-# print(os.listdir('../../../'))
-# print(os.listdir('../'))
-# print(os.listdir('./variant_calling/strelka/HCC1395N/'))
+filename = "HCC1395N.strelka.genome.vcf.gz"
+path = "./variant_calling/strelka/HCC1395N/%s" % filename
+with open(path, "rb") as fp:
+    r = requests.put(
+        "%s/%s" % (bucket_url, filename),
+        data=fp,
+        params=params,
+    )
+r.json()
 
-# bucket_url = "https://zenodo.org/api/deposit/depositions"
-# with open(path, "rb") as fp:
-#     r = requests.post(
-#         "%s/%s" % (bucket_url, filename),
-#         data=fp,
-#         params=params,
-#     )
-# r.json()
+# Add metadata to uploaded file
 
 data = {
     'metadata': {
@@ -38,11 +42,18 @@ data = {
     }
 }
 
-r = requests.post(url, data=json.dumps(data), headers=headers)
+r = requests.put('https://sandbox.zenodo.org/api/deposit/depositions/%s' % deposition_id,
+                params=params, data=json.dumps(data),
+                headers=headers)
 r.status_code
-# 200
 
-r.json()["metadata"]["recid"]
+
+# filename = "HCC1395N.strelka.genome.vcf.gz"
+# path = "./variant_calling/strelka/HCC1395N/%s" % filename
+# print(os.listdir('../../'))
+# print(os.listdir('../../../'))
+# print(os.listdir('../'))
+# print(os.listdir('./variant_calling/strelka/HCC1395N/'))
 
 os.environ['DEPOSITION_ID'] = r.json()["metadata"]["recid"]
 # TODO add publication step
