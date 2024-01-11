20fc10bad6fca0f8ff3cd3b31af3b31f0359222d
FriederikeHanssen <Friederike.hanssen@qbic.uni-tuebingen.de>
2024-01-11 15:33:58 +0000

    replace more curly brackets

--- .github/workflows/set_ncbench_config.sh
+++ .github/workflows/set_ncbench_config.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
 
-for reads in 75 200; do
+for READS_ in 75 200; do
     yq --inplace '
-        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${reads}M.labels;
+        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-$READS_M.labels;
         .site = "nf-core" |
         .pipeline = "nf-core/sarek v$PIPELINE_VERSION" |
         .trimming = "FastP v${FASTP_VERSION}" |
@@ -12,15 +12,15 @@ for reads in 75 200; do
         .realignment = "none" |
         .variant-detection  = "strelka2 v${STRELKA_VERSION}" |
         .genotyping = "none" |
-        .reads = "${reads}M" ) |
-        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${reads}M.subcategory;
+        .reads = "$READS_M" ) |
+        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-$READS_M.subcategory;
         . = "NA12878-agilent" ) |
-        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${reads}M.zenodo;
+        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-$READS_M.zenodo;
         .deposition = $DEPOSITION_ID  |
         .filename = "TODO get here proper file names" ) |
-        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${reads}M.benchmark;
-        . = "giab-NA12878-agilent-${reads}M" ) |
-        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${reads}M.rename-contigs;
+        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-$READS_M.benchmark;
+        . = "giab-NA12878-agilent-$READS_M" ) |
+        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-$READS_M.rename-contigs;
         . = "resources/rename-contigs/ucsc-to-ensembl.txt" )
         ' ncbench-workflow/config/config.yaml
 done
