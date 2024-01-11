d9486f32ab8dbd8bc57876c7ee980e5cf4d88e25
FriederikeHanssen <Friederike.hanssen@qbic.uni-tuebingen.de>
2024-01-11 15:42:00 +0000

    too many curly braces

--- .github/workflows/set_ncbench_config.sh
+++ .github/workflows/set_ncbench_config.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
 
-for {READS_} in 75 200; do
+for READS in 75 200; do
     yq --inplace "
-        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${READS_}M.labels;
+        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${READS}M.labels;
         .site = "nf-core" |
         .pipeline = "nf-core/sarek v${PIPELINE_VERSION}" |
         .trimming = "FastP v${FASTP_VERSION}" |
@@ -12,15 +12,15 @@ for {READS_} in 75 200; do
         .realignment = "none" |
         .variant-detection  = "strelka2 v${STRELKA_VERSION}" |
         .genotyping = "none" |
-        .reads = "${READS_}M" ) |
-        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${READS_}M.subcategory;
+        .reads = "${READS}M" ) |
+        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${READS}M.subcategory;
         . = "NA12878-agilent" ) |
-        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${READS_}M.zenodo;
+        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${READS}M.zenodo;
         .deposition = ${DEPOSITION_ID}  |
         .filename = "TODO get here proper file names" ) |
-        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${READS_}M.benchmark;
-        . = "giab-NA12878-agilent-${READS_}M" ) |
-        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${READS_}M.rename-contigs;
+        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${READS}M.benchmark;
+        . = "giab-NA12878-agilent-${READS}M" ) |
+        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${READS}M.rename-contigs;
         . = "resources/rename-contigs/ucsc-to-ensembl.txt" )
         " ncbench-workflow/config/config.yaml
 done
