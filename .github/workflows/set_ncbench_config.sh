46abe991b36bdba94fbada5af8ef66d40832fd83
FriederikeHanssen <Friederike.hanssen@qbic.uni-tuebingen.de>
2024-01-11 18:56:24 +0000

    try again one env after another

--- .github/workflows/set_ncbench_config.sh
+++ .github/workflows/set_ncbench_config.sh
@@ -9,6 +9,11 @@
         # .base-quality-recalibration = "gatk4 v$GATK_VERSION" |
         # .realignment = "none" |
         # .variant-detection  = "strelka2 v$STRELKA_VERSION" |
+
+        #with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.subcategory;
+        #. = "NA12878-agilent" ) |
+        #with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.benchmark;
+        #. = "giab-NA12878-agilent-${READS}M" ) |
 for READS in 75 200; do
     yq --inplace "
         with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.labels;
@@ -16,10 +21,6 @@ for READS in 75 200; do
         .pipeline = "nf-core/sarek v$PIPELINE_VERSION" |
         .genotyping = "none" |
         .reads = "${READS}M" ) |
-        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.subcategory;
-        . = "NA12878-agilent" ) |
-        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.benchmark;
-        . = "giab-NA12878-agilent-${READS}M" ) |
         with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.rename-contigs;
         . = "resources/rename-contigs/ucsc-to-ensembl.txt" )
         " ncbench-workflow/config/config.yaml
