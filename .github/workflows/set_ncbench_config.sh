46f068929f3d15232b15088c3bd96049b87f9098
FriederikeHanssen <Friederike.hanssen@qbic.uni-tuebingen.de>
2024-01-11 16:06:59 +0000

    maybe just the deopisiton id has issues

--- .github/workflows/set_ncbench_config.sh
+++ .github/workflows/set_ncbench_config.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-#        .trimming = "FastP v${FASTP_VERSION}" |
-#        .read-mapping = "bwa mem v${BWA_VERSION}" |
-#        .base-quality-recalibration = "gatk4 v${GATK_VERSION}" |
-#        .realignment = "none" |
-#        .variant-detection  = "strelka2 v${STRELKA_VERSION}" |
-#        .genotyping = "none" |
+
+        #with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.zenodo;
+        #.deposition = $DEPOSITION_ID  |
+        #.filename = "TODO get here proper file names" ) |
 for READS in 75 200; do
     yq --inplace "
         with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.labels;
         .site = "nf-core" |
         .pipeline = "nf-core/sarek v$PIPELINE_VERSION" |
+        .trimming = "FastP v${FASTP_VERSION}" |
+        .read-mapping = "bwa mem v${BWA_VERSION}" |
+        .base-quality-recalibration = "gatk4 v${GATK_VERSION}" |
+        .realignment = "none" |
+        .variant-detection  = "strelka2 v${STRELKA_VERSION}" |
+        .genotyping = "none" |
         .reads = "${READS}M" ) |
         with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.subcategory;
         . = "NA12878-agilent" ) |
-        with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.zenodo;
-        .deposition = $DEPOSITION_ID  |
-        .filename = "TODO get here proper file names" ) |
         with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.benchmark;
         . = "giab-NA12878-agilent-${READS}M" ) |
         with(.variant-calls.nf-core-sarek-$PIPELINE_VERSION-strelka-agilent-${READS}M.rename-contigs;
