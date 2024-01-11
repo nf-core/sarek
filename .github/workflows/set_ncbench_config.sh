0bbce6b4968a72599be0a4eae1bfee8003ace9f2
FriederikeHanssen <Friederike.hanssen@qbic.uni-tuebingen.de>
2024-01-11 15:20:19 +0000

    call new variable

--- .github/workflows/set_ncbench_config.sh
+++ .github/workflows/set_ncbench_config.sh
@@ -3,24 +3,24 @@
 
 for reads in 75 200; do
     yq --inplace '
-        with(.variant-calls.nf-core-sarek-${{ env.PIPELINE_VERSION }}-strelka-agilent-${reads}M.labels;
+        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${reads}M.labels;
         .site = "nf-core" |
-        .pipeline = "nf-core/sarek v${{ env.PIPELINE_VERSION }}" |
-        .trimming = "FastP v${{ env.FASTP_VERSION }}" |
-        .read-mapping = "bwa mem v${{ env.BWA_VERSION }}" |
-        .base-quality-recalibration = "gatk4 v${{ env.GATK_VERSION }}" |
+        .pipeline = "nf-core/sarek v${PIPELINE_VERSION}" |
+        .trimming = "FastP v${FASTP_VERSION}" |
+        .read-mapping = "bwa mem v${BWA_VERSION}" |
+        .base-quality-recalibration = "gatk4 v${GATK_VERSION}" |
         .realignment = "none" |
-        .variant-detection  = "strelka2 v${{ env.STRELKA_VERSION }}" |
+        .variant-detection  = "strelka2 v${STRELKA_VERSION}" |
         .genotyping = "none" |
         .reads = "${reads}M" ) |
-        with(.variant-calls.nf-core-sarek-${{ env.PIPELINE_VERSION }}-strelka-agilent-${reads}M.subcategory;
+        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${reads}M.subcategory;
         . = "NA12878-agilent" ) |
-        with(.variant-calls.nf-core-sarek-${{ env.PIPELINE_VERSION }}-strelka-agilent-${reads}M.zenodo;
+        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${reads}M.zenodo;
         .deposition = $DEPOSITION_ID  |
         .filename = "TODO get here proper file names" ) |
-        with(.variant-calls.nf-core-sarek-${{ env.PIPELINE_VERSION }}-strelka-agilent-${reads}M.benchmark;
+        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${reads}M.benchmark;
         . = "giab-NA12878-agilent-${reads}M" ) |
-        with(.variant-calls.nf-core-sarek-${{ env.PIPELINE_VERSION }}-strelka-agilent-${reads}M.rename-contigs;
+        with(.variant-calls.nf-core-sarek-${PIPELINE_VERSION}-strelka-agilent-${reads}M.rename-contigs;
         . = "resources/rename-contigs/ucsc-to-ensembl.txt" )
         ' ncbench-workflow/config/config.yaml
 done
