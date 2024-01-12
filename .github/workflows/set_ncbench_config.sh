5fe04a379d076dc64801767d5dbc17e8c654166e
FriederikeHanssen <Friederike.hanssen@qbic.uni-tuebingen.de>
2024-01-12 10:53:26 +0000

    let's see fif copilot is right

--- .github/workflows/set_ncbench_config.sh
+++ .github/workflows/set_ncbench_config.sh
@@ -2,26 +2,26 @@
 
     for READS in 75 200; do
         yq --inplace '
-                with(.variant-calls.nf-core-sarek-123-strelka-agilent-75M.labels;
-                .site = "nf-core" |
-                .pipeline = "nf-core/sarek v123" |
-                .trimming = "FastP v123" |
-                .read-mapping = "bwa mem v123" |
-                .base-quality-recalibration = "gatk4 v123" |
-                .realignment = "none" |
-                .variant-detection  = "strelka2 v123" |
-                .genotyping = "none" |
-                .reads = "75M" ) |
-                with(.variant-calls.nf-core-sarek-123-strelka-agilent-75M.subcategory;
-                . = "NA12878-agilent" ) |
-                with(.variant-calls.nf-core-sarek- 123-strelka-agilent-75M.zenodo;
-                .deposition = 123  |
-                .filename = "get here proper file names" ) |
-                with(.variant-calls.nf-core-sarek- 123-strelka-agilent-75M.benchmark;
-                . = "giab-NA12878-agilent-75M" ) |
-                with(.variant-calls.nf-core-sarek- 123-strelka-agilent-75M.rename-contigs;
-                . = "resources/rename-contigs/ucsc-to-ensembl.txt" )
-                ' ncbench-workflow/config/config.yaml
+            with(.variant-calls.nf-core-sarek-123-strelka-agilent-'"${READS}"'M.labels;
+            .site = "nf-core" |
+            .pipeline = "nf-core/sarek v123" |
+            .trimming = "FastP v123" |
+            .read-mapping = "bwa mem v123" |
+            .base-quality-recalibration = "gatk4 v123" |
+            .realignment = "none" |
+            .variant-detection  = "strelka2 v123" |
+            .genotyping = "none" |
+            .reads = "'"${READS}"'M" ) |
+            with(.variant-calls.nf-core-sarek-123-strelka-agilent-'"${READS}"'M.subcategory;
+            . = "NA12878-agilent" ) |
+            with(.variant-calls.nf-core-sarek-123-strelka-agilent-'"${READS}"'M.zenodo;
+            .deposition = 123  |
+            .filename = "get here proper file names" ) |
+            with(.variant-calls.nf-core-sarek-123-strelka-agilent-'"${READS}"'M.benchmark;
+            . = "giab-NA12878-agilent-'"${READS}"'M" ) |
+            with(.variant-calls.nf-core-sarek-123-strelka-agilent-'"${READS}"'M.rename-contigs;
+            . = "resources/rename-contigs/ucsc-to-ensembl.txt" )
+            '  ncbench-workflow/config/config.yaml
     done
 
 
