bd18908e1c3e301e0cc87cce708cbde50de751aa
FriederikeHanssen <Friederike.hanssen@qbic.uni-tuebingen.de>
2024-01-12 11:09:54 +0000

    expand all other variables

--- .github/workflows/set_ncbench_config.sh
+++ .github/workflows/set_ncbench_config.sh
@@ -4,18 +4,18 @@
         yq --inplace '
             with(.variant-calls.nf-core-sarek-'"${PIPELINE_VERSION_NO_DOTS}"'-strelka-agilent-'"${READS}"'M.labels;
             .site = "nf-core" |
-            .pipeline = "nf-core/sarek v123" |
-            .trimming = "FastP v123" |
-            .read-mapping = "bwa mem v123" |
-            .base-quality-recalibration = "gatk4 v123" |
+            .pipeline = "nf-core/sarek v'"${PIPELINE_VERSION}"'" |
+            .trimming = "FastP v'"${FASTP_VERSION}"'" |
+            .read-mapping = "bwa mem v'"${BWA_VERSION}"'" |
+            .base-quality-recalibration = "gatk4 v'"${GATK_VERSION}"'" |
             .realignment = "none" |
-            .variant-detection  = "strelka2 v123" |
+            .variant-detection  = "strelka2 v'"${STRELKA_VERSION}"'" |
             .genotyping = "none" |
             .reads = "'"${READS}"'M" ) |
             with(.variant-calls.nf-core-sarek-'"${PIPELINE_VERSION_NO_DOTS}"'-strelka-agilent-'"${READS}"'M.subcategory;
             . = "NA12878-agilent" ) |
             with(.variant-calls.nf-core-sarek-'"${PIPELINE_VERSION_NO_DOTS}"'-strelka-agilent-'"${READS}"'M.zenodo;
-            .deposition = 123  |
+            .deposition = '"${PIPELINE_VERSION_NO_DOTS}"'  |
             .filename = "get here proper file names" ) |
             with(.variant-calls.nf-core-sarek-'"${PIPELINE_VERSION_NO_DOTS}"'-strelka-agilent-'"${READS}"'M.benchmark;
             . = "giab-NA12878-agilent-'"${READS}"'M" ) |
