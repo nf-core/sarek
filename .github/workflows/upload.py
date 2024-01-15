19380d5b61a75f0cfbf11e21267fa66d7b520d3d
FriederikeHanssen <Friederike.hanssen@qbic.uni-tuebingen.de>
2024-01-15 12:34:40 +0000

    linter

--- .github/workflows/upload.py
+++ .github/workflows/upload.py
@@ -12,31 +12,31 @@ pipeline_version = os.environ["PIPELINE_VERSION"]
 url =  f"https://sandbox.zenodo.org/api/deposit/depositions"
 
 # Create empty upload
-r = requests.post(url,
-                params=params,
-                json={},
-                headers=headers)
+r = requests.post(url, params=params, json={}, headers=headers)
 
 print("Create empty upload:\n")
 print(r.json())
+print(r.status_code)
 
 deposition_id = r.json()["id"]
 
 ## Store deposition ID
-with open('deposition_id.txt', 'w') as f:
+with open("deposition_id.txt", 'w') as f:
     f.write(str(deposition_id))
 
 # Upload a new file
 bucket_url = r.json()["links"]["bucket"]
 
-filenames = [ "deepvariant/NA12878_75M/NA12878_75M.deepvariant.vcf.gz",
-            "freebayes/NA12878_75M/NA12878_75M.freebayes.vcf.gz",
-            "haplotypecaller/NA12878_75M/NA12878_75M.haplotypecaller.filtered.vcf.gz",
-            "strelka/NA12878_75M/NA12878_75M.strelka.variants.vcf.gz",
-            "deepvariant/NA12878_200M/NA12878_200M.deepvariant.vcf.gz",
-            "freebayes/NA12878_200M/NA12878_200M.freebayes.vcf.gz",
-            "haplotypecaller/NA12878_200M/NA12878_200M.haplotypecaller.filtered.vcf.gz",
-            "strelka/NA12878_200M/NA12878_200M.strelka.variants.vcf.gz"]
+filenames = [
+    "deepvariant/NA12878_75M/NA12878_75M.deepvariant.vcf.gz",
+    "freebayes/NA12878_75M/NA12878_75M.freebayes.vcf.gz",
+    "haplotypecaller/NA12878_75M/NA12878_75M.haplotypecaller.filtered.vcf.gz",
+    "strelka/NA12878_75M/NA12878_75M.strelka.variants.vcf.gz",
+    "deepvariant/NA12878_200M/NA12878_200M.deepvariant.vcf.gz",
+    "freebayes/NA12878_200M/NA12878_200M.freebayes.vcf.gz",
+    "haplotypecaller/NA12878_200M/NA12878_200M.haplotypecaller.filtered.vcf.gz",
+    "strelka/NA12878_200M/NA12878_200M.strelka.variants.vcf.gz",
+]
 
 for file in filenames:
     path = "./variant_calling/%s" % file
@@ -46,23 +46,29 @@ for file in filenames:
             data=fp,
             params=params,
         )
+        print(r.json())
 
 # Add metadata to uploaded file
 title = 'WES benchmark results nf-core/sarek v{}'.format(pipeline_version)
 data = {
-    'metadata': {
-        'title': title,
-        'upload_type': 'data',
-        'description': 'Variant calling results on benchmarking datasets produced with nf-core/sarek',
-        'creators': [{'name': 'Garcia, Maxime Ulysse', 'affiliation': 'Seqera, Barcelona'},
-                    {'name': 'Hanssen, Friederike', 'affiliation': 'Quantitative Biology Center, Tuebingen'}]
+    "metadata": {
+        "title": title,
+        "upload_type": "data",
+        "description": "Variant calling results on benchmarking datasets produced with nf-core/sarek",
+        "creators": [
+            {"name": "Garcia, Maxime Ulysse", "affiliation": "Seqera, Barcelona"},
+            {"name": "Hanssen, Friederike", "affiliation": "Quantitative Biology Center, Tuebingen"},
+        ],
     }
 }
+
 # TODO replace sandbox link https://zenodo.org/api/deposit/depositions/
-r = requests.put('https://sandbox.zenodo.org/api/deposit/depositions/%s' % deposition_id,
-                params=params,
-                data=json.dumps(data),
-                headers=headers)
+r = requests.put(
+    'https://sandbox.zenodo.org/api/deposit/depositions/%s' % deposition_id,
+    params=params,
+    data=json.dumps(data),
+    headers=headers,
+)
 
 print("Add metadata: ")
 print(r.status_code)
@@ -70,8 +76,9 @@ print(r.json())
 
 # TODO only uncomment once everything works, replace sandbox link
 # Publish this
-r = requests.post('https://sandbox.zenodo.org/api/deposit/depositions/%s/actions/publish' % deposition_id,
-                    params=params )
+r = requests.post(
+    'https://sandbox.zenodo.org/api/deposit/depositions/%s/actions/publish' % deposition_id, params=params
+)
 
 print("Publish data status code: ")
 print(r.status_code)
