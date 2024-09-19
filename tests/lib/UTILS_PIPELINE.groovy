3e3c5ed3f194b49c2b2e74540eba98331106bb98
maxulysse <max.u.garcia@gmail.com>
2024-09-19 17:11:59 +0200

    better regexes

--- tests/lib/UTILS_PIPELINE.groovy
+++ tests/lib/UTILS_PIPELINE.groovy
@@ -4,6 +4,28 @@ class UTILS_PIPELINE {
         // To exclude the pipeline_software_mqc_versions.yml file that contains the Nextflow version
         /nf_core_.*_software_mqc_versions\.yml/,
 
+        // To exclude csv files created by the pipeline that contains absolute paths
+        /(markduplicates|markduplicates_no_table|recalibrated|variantcalled)\.csv/,
+
+        // To exclude from multiqc
+        /multiqc\.log/,
+        /multiqc_data\.json/,
+        /multiqc_sources\.txt/,
+        /multiqc_report\.html/,
+
+        // To exclude cram and index files
+        /.*\.(md|recal)\.cram/,
+
+        // To exclude markduplicates metrics
+        /.*\.md\.cram\.metrics/,
+
+        // To exclude vcf and index files
+        /.*\.vcf\.gz/,
+        /.*\.vcf\.gz\.tbi/,
+
+        // To exclude fastqc
+        /.*_fastq\.(html|zip)/,
+
         // To exclude trimgalore
         /.*\.fastq\.gz_trimming_report\.txt/
     ]
